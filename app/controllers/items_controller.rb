64d6ac08408fa993915e5a2b47003bdd8de5ad50
yijordan <yij@uchicago.edu>
2025-02-28 02:06:02 +0000

    moved the item.all call to the controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def homepage
+    @list_of_items = Item.all.order({ :created_at => :desc })
     render({ :template => "items_templates/homepage" })
   end
 end
